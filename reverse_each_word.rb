830730299f9763ec764848ee4a8e6705187349e9
Michael Bramblett <michaelbramblett0@gmail.com>
2019-12-11 01:39:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
  
-def reverse_each_word(string)
+def reverse_each_word("Hello there, and how are you?")
   string.each do |words|
-  puts "Hello there, and how are you?"
+  # puts "Hello there, and how are you?"
   end
 end
