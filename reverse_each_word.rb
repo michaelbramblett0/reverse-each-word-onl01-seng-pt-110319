f0745edc7a34fffbe46f042fcdb6604baf21ab25
Michael Bramblett <michaelbramblett0@gmail.com>
2019-12-11 01:56:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence1 = "Hello there, and how are you?"
  
-def reverse_each_word(sentence1)
+ def reverse_each_word(sentence1)
   memo = ''
   sentence1.split.each { |joined_array| memo << "#{joined_array.reverse}" }
   end
