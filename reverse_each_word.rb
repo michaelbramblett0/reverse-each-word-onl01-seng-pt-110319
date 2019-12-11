e1e9e90443cca242e4b8afbe9c46eab38cbdd157
Michael Bramblett <michaelbramblett0@gmail.com>
2019-12-11 01:54:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
  
 def reverse_each_word(sentence1)
-memo = ''
-sentence1.split.each { |joined_array| memo << "#{joined_array.reverse}" }
+  memo = ''
+  sentence1.split.each { |joined_array| memo << "#{joined_array.reverse}" }
   end
 end
